0cf4d0fcc7c7d9f9e0563916e9042403bde4fec6
Javeria Khan <javeriak@plumgrid.com>
2016-01-08 08:45:08 -0800

    Missing fabric address
    Signed-off-by: Javeria Khan <javeriak@plumgrid.com>

--- deployment_scripts/plumgrid_fabric.sh
+++ deployment_scripts/plumgrid_fabric.sh
@@ -42,7 +42,7 @@ if [[ -f "/etc/network/interfaces.d/ifcfg-$fabric_dev" ]];then
   rm /etc/network/interfaces.d/ifcfg-$fabric_dev
 fi
 
-echo -e "auto $fabric_dev\niface $fabric_dev inet static\nddress $fabric_net.$fabric_ip/24\nmtu 1580" >> /etc/network/interfaces.d/ifcfg-$fabric_dev
+echo -e "auto $fabric_dev\niface $fabric_dev inet static\naddress $fabric_net.$fabric_ip/24\nmtu 1580" >> /etc/network/interfaces.d/ifcfg-$fabric_dev
 
 grep -q -F "fabric_dev: $fabric_dev" /etc/astute.yaml || echo "fabric_dev: $fabric_dev" >> /etc/astute.yaml
                                                                                                                            
