e0af566571362132b7bbf0d418abfff832f215aa
Javeria Khan <javeriak@plumgrid.com>
2016-01-22 11:15:17 -0800

    Update libvirt version
    Signed-off-by: Javeria Khan <javeriak@plumgrid.com>

--- deployment_scripts/puppet/manifests/pre_deployment.pp
+++ deployment_scripts/puppet/manifests/pre_deployment.pp
@@ -16,8 +16,8 @@
 notice('MODULAR: plumgrid/pre_deployment.pp')
 
 package { 'libvirt0' :
-  ensure => '1.2.2-0ubuntu13.1.14' ,
+  ensure => '1.2.2-0ubuntu13.1.16' ,
 } ->
 package { 'libvirt-bin' :
-  ensure => '1.2.2-0ubuntu13.1.14' ,
+  ensure => '1.2.2-0ubuntu13.1.16' ,
 }
