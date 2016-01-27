eccc46aa5501d0d9990f7c2f0a02b835de141173
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2016-01-27 11:30:05 +0100

    Do not pin specific libvirt version
    The latest version of libvirt on Trusty is 1.2.2-0ubuntu13.1.16. Instead
    of requiring specific version, let's try to pull the latest and hope
    for the best.
    
    Puppet doesn't have possibility to ensure the minimum given version,
    unfortunately neither does apt itself.

--- deployment_scripts/puppet/manifests/pre_deployment.pp
+++ deployment_scripts/puppet/manifests/pre_deployment.pp
@@ -15,9 +15,6 @@
 
 notice('MODULAR: plumgrid/pre_deployment.pp')
 
-package { 'libvirt0' :
-  ensure => '1.2.2-0ubuntu13.1.14' ,
-} ->
-package { 'libvirt-bin' :
-  ensure => '1.2.2-0ubuntu13.1.14' ,
+package { ['libvirt0', 'libvirt-bin']:
+  ensure => 'latest',
 }
